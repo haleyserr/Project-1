baeb0883a3ce519049d4a6527c46bfd9beb25eb6
danman15726 <danman15726@gmail.com>
2019-11-07 14:03:06 -0600

    Added basic starting points with the project such as: bootstrap, firebase, jquery, javascript, and css. Now going to start on APIs and finding them. OUT.

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+apiKey: "AIzaSyDHTBkbZBnyqTui3WPd6TfbYm9P4YmrSEg",
+authDomain: "operation-t-a-s-k-3ff16.firebaseapp.com",
+databaseURL: "https://operation-t-a-s-k-3ff16.firebaseio.com",
+projectId: "operation-t-a-s-k-3ff16",
+storageBucket: "operation-t-a-s-k-3ff16.appspot.com",
+messagingSenderId: "103599166251",
+appId: "1:103599166251:web:7b8f0cb7116749241dab6e",
+measurementId: "G-8507VK2V5L"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+const database  = firebase.database();
+
+
+
+
+
+
+
+
+console.log("APP.JS WORKS");
+
+
+
+
+
