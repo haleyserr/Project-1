95d67d88d8cafdf85233d68fc48219f6b3b983c3
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-11-09 10:48:14 -0600

    Added History questions mostly

---
+++ gameNathan/app.js
@@ -0,0 +1,33 @@
+const questions = [{
+    question: "When did Christopher Columbus sail across the Atlantic Ocean?",
+    answer: "1492",
+},
+{
+    question: "Who is the president on the quarter?",
+    answer: "George Washington",
+},
+{
+    question: "Who was the 10th president of the United States?",
+    answer: "John Tyler",
+},
+{
+    question: "What war was fought between the North and South sections of The United States?",
+    answer: "Civil War",
+},
+{
+    question: "What were the first 10 amendments of the Constitution called?",
+    answer: "Bill of Rights",
+},
+{
+    question: "What group of settlers in the Americas first celebrated Thanksgiving?",
+    answer: "Pilgrims",
+},
+{
+    question: "Who is the President on the half dollar that was assasinated in Dallas, Texas?",
+    answer: "John F. Kennedy",
+},
+{
+    question: "Who did the United States buy the Lousiana Purchase from?",
+    answer: "Napoleon",
+},
+];
