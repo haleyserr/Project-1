1fec14d950f2da7e8f7688807c39652a85a1bdb5
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-11-09 11:07:25 -0600

    added basic card for html, added fake answers for some questions

--- gameNathan/app.js
+++ gameNathan/app.js
@@ -1,14 +1,24 @@
+let correct = 0;
 const questions = [{
     question: "When did Christopher Columbus sail across the Atlantic Ocean?",
     answer: "1492",
+    fake: "1512",
+    fake2: "1812",
+    fake3: "1792",
 },
 {
     question: "Who is the president on the quarter?",
     answer: "George Washington",
+    fake: "Abraham Lincoln",
+    fake2: "Franklin D. Roosevelt",
+    fake3: "George Bush",
 },
 {
     question: "Who was the 10th president of the United States?",
     answer: "John Tyler",
+    fake: "Abraham Lincoln",
+    fake2: "John Adams",
+    fake3: "George Washington",
 },
 {
     question: "What war was fought between the North and South sections of The United States?",
@@ -31,3 +41,7 @@ const questions = [{
     answer: "Napoleon",
 },
 ];
+
+function pagePrep (){
+
+}
