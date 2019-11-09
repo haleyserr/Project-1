9c8f8b8b371a96c93cf3812763b6aae0954947bf
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-11-09 11:12:42 -0600

    Finished creating fake answers for questions

--- gameNathan/app.js
+++ gameNathan/app.js
@@ -23,22 +23,37 @@ const questions = [{
 {
     question: "What war was fought between the North and South sections of The United States?",
     answer: "Civil War",
+    fake: "War of 1812",
+    fake2: "World War I",
+    fake3: "Indian War",
 },
 {
     question: "What were the first 10 amendments of the Constitution called?",
     answer: "Bill of Rights",
+    fake: "10 commandments",
+    fake2: "Bill of Rites",
+    fake3: "Doctorine of human rights",
 },
 {
     question: "What group of settlers in the Americas first celebrated Thanksgiving?",
     answer: "Pilgrims",
+    fake: "Indians",
+    fake2: "French",
+    fake3: "British",
 },
 {
     question: "Who is the President on the half dollar that was assasinated in Dallas, Texas?",
     answer: "John F. Kennedy",
+    fake: "Abraham Lincoln",
+    fake2: "James A. Garfield",
+    fake3: "William McKinley",
 },
 {
     question: "Who did the United States buy the Lousiana Purchase from?",
-    answer: "Napoleon",
+    answer: "Napoleon Bonaparte",
+    fake: "Napolean Dynamite",
+    fake2: "Julius Caesar",
+    fake3: "King James",
 },
 ];
 
