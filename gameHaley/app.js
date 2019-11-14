16a382c46ea3618ea61033183446e77f6289f5a5
Haley Serr <haleykpelletier@gmail.com>
2019-11-13 20:52:39 -0600

    Edited Cards on Main Screen, Setup Basic Dashboard Tables, Updated ReadMe with Deployed Link

--- gameHaley/app.js
+++ gameHaley/app.js
@@ -117,7 +117,27 @@ $(document).ready(function () {
 
     // Update Firebase with score when quiz complete
 
-   
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-analytics.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-database.js"></script>
+    
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyBiKzQFqt-WPMEGbNOMbL4RhTIeXXWqV_M",
+        authDomain: "operation-t-a-s-k-d028a.firebaseapp.com",
+        databaseURL: "https://operation-t-a-s-k-d028a.firebaseio.com",
+        projectId: "operation-t-a-s-k-d028a",
+        storageBucket: "operation-t-a-s-k-d028a.appspot.com",
+        messagingSenderId: "520491246095",
+        appId: "1:520491246095:web:b686d6f2add5af5a88b97a",
+        measurementId: "G-KS570N29FC"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    
+    const database = firebase.database();
 
 
 
